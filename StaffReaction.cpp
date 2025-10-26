0f9c00fd68cafc9e77088883fabb882c1af0bb14
JordanUP <u24597181@tuks.co.za>
2025-10-26 20:17:37 +0200

    Delete StaffReaction.cpp

--- StaffReaction.cpp
+++
@@ -1,7 +0,0 @@
-#include "StaffReaction.h"
-
-StaffReaction::StaffReaction(std::string action) : action_(action) {}
-
-void StaffReaction::execute(std::string type) {
-    std::cout << "Executing " << action_ << " for " << type << std::endl;
-}
